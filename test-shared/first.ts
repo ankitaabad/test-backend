2214ef30c81af81dfd85d4058e65ca8c8c41dac4
ankit <ankitaabad.1@gmail.com>
2025-03-30 16:35:35 +0530

    git subrepo clone https://github.com/ankitaabad/test-shared.git
    subrepo:
      subdir:   "test-shared"
      merged:   "73c2384"
    upstream:
      origin:   "https://github.com/ankitaabad/test-shared.git"
      branch:   "main"
      commit:   "73c2384"
    git-subrepo:
      version:  "0.4.9"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "30db3b8"

---
+++ test-shared/first.ts
@@ -0,0 +1,16 @@
+export type user = {
+  name: string;
+  city: string;
+  age: number;
+};
+
+export type photo = {
+  id: string;
+  tag: string;
+  url: string;
+};
+
+export type follower = {
+  id: string;
+  name: string;
+};
